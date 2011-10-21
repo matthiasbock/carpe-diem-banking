5cd0609de2880d5ff1024a6f397eb8b2d2283771
matthias.bock <matthias.bock@hu-berlin.de>
2011-10-21 10:41:38 +0000

    Backups als tags/ hochgeladen

---
+++ main/Klient.py
@@ -0,0 +1,134 @@
+# -*- coding: iso-8859-15 -*-
+
+from Kontenverwaltung.main.models import *
+from Kontenverwaltung.main.includes import *
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.cache import cache_control
+from django.contrib.auth.decorators import login_required
+
+import datetime
+from operator import itemgetter
+
+@cache_control( max_age=1, must_revalidate=True, no_cache=True )
+#login_required...
+def Klient( request ):			# Umsätze des Klienten anzeigen
+	params = defaults( request )
+	params["Anzahl"] = 0
+	try:
+		Klient = request.GET.get("Klient")
+	except:
+		Klient = None
+	if Klient == None:
+		return HttpResponseRedirect("Intranet")
+	Klient = Klienten.objects.get( id=Klient )
+	params["Startdatum"] = Klient.startdatum
+	if Klient.startsaldo != None:
+		params["Startsaldo"] = currency( Klient.startsaldo )
+		params["Saldo"] = Klient.startsaldo
+	
+	# alle Salden aufzählen
+	params["Anzahl"] = 0
+	params["Ergebnisse"] = []
+	
+	# Zuteilungen
+	for E in Zuteilungen.objects.filter( anklient = Klient.id ):
+		B = Betreuer.objects.get( id=E.betreuer )
+		params["Ergebnisse"].append( {"typ":"Zuteilung", "id":E.id, "datum":E.datum, "vorgang":"Zuteilung, "+B.vorname+" "+B.nachname, "betrag":currency( E.teilbetrag ) } )
+		params["Anzahl"] += 1
+		params["Saldo"] += E.betrag
+	
+	# Einzahlungen
+#	for Einzahlung in Klienteneinzahlungen.objects.filter( klient = Klient.id ):
+	
+	# Auszahlungen
+	for E in Klientenauszahlungen.objects.filter( anklient = Klient.id ):
+		B = Betreuer.objects.get( id=E.betreuer )
+		K = Klientenkassen.objects.get( id=E.ausklientenkasse )
+		params["Ergebnisse"].append( {"typ":"Auszahlung", "id":E.id, "datum":E.datum, "vorgang":"Barauszahlung, "+B.vorname+" "+B.nachname+"<br/><i>"+K.name+"</i>", "betrag":currency( -E.betrag ) } )
+		params["Anzahl"] += 1
+		params["Saldo"] -= E.betrag
+	
+	# Überweisungen
+	_Ueberweisungen = Klientenueberweisungen.objects.filter( fuerklient = Klient.id )
+	
+	# nach Datum ordnen
+	get = itemgetter('datum')
+	params["Ergebnisse"].sort(key = get)
+
+	if Klient.startsaldo != None:
+		params["Saldo"] = currency( params["Saldo"] )
+	
+	params["Forderungsanzahl"] = 0
+	params["Forderungen"] = Schulden.objects.filter( klient = Klient.id )
+	params["Restschuld"] = currency( 0 )
+	return render_to_response("Klient.html", params)
+
+def Forderungen( request ):
+	params = defaults( request )
+	return render_to_response("Baustelle.html", params)
+
+def Einzahlung( request ):
+	params = defaults( request )
+	return render_to_response("Baustelle.html", params)
+
+def Auszahlung( request ):
+	params = defaults( request )
+	if request.method == "GET":		# Seite: Auszahlung anzeigen
+		try:
+			Klient = int(request.GET.get("Klient"))
+		except:
+			Klient = None
+		if Klient == None or Klient == 0:
+			setsession( request, "Fehler", "Klienten-ID wurde nicht an das Auszahlungsformular übergeben ! Bitte Administrator kontaktieren." )
+			return HttpResponseRedirect("Intranet")
+		return render_to_response("Auszahlung.html", params)
+	elif request.method == "POST":		# Auszahlung speichern
+		try:
+			Klient = int(request.POST.get("Klient"))
+		except:				# haben wir einen Parameter Klient bekommen ?
+			Klient = None
+		if Klient == None or Klient == 0:
+			setsession( request, "Fehler", "Klienten-ID wurde nicht an den Auszahlungsprozess übergeben ! Bitte Administrator kontaktieren." )
+			return HttpResponseRedirect("Intranet")
+		B = Betreuer.objects.get( auth_user_id=request.user.id )
+		try:
+			Betrag = request.POST.get("Betrag").replace(",",".")
+			if float(Betrag) <= 0:	# ist es ein gültiger Betrag ?
+				raise
+		except:
+			setsession( request, "Fehler", "Das ist kein gültiger Betrag ! Auszahlung nicht gespeichert." )
+			return HttpResponseRedirect("Klient?Klient="+str(Klient))
+						# Auszahlung speichern !
+		Klientenauszahlungen.objects.create( datum=datetime.date.today(), betreuer=B.id, betrag=Betrag, ausklientenkasse=B.klientenkasse, anklient=Klient )
+		setsession( request, "Nachricht", "Auszahlung wurde gespeichert." )
+		return HttpResponseRedirect("Klient?Klient="+str(Klient))
+
+def Ueberweisung( request ):
+	params = defaults( request )
+	return render_to_response("Baustelle.html", params)
+
+def NeueSchulden( request ):
+	params = defaults( request )
+	return render_to_response("Baustelle.html", params)
+
+def Entfernen( request ):
+	try:
+		returnto = request.GET.get("returnto")
+	except:
+		returnto = ""
+	if returnto == "":
+		returnto = "Intranet"
+	try:
+		Typ = request.GET.get("Typ")
+		ID = request.GET.get("ID")
+	except:
+		return HttpResponseRedirect( returnto )
+	if Typ == "Auszahlung":
+		try:
+			Klientenauszahlungen.objects.get( id=ID ).delete()
+			setsession( request, "Nachricht", "Auszahlung wurde gelöscht." )
+		except:
+			setsession( request, "Fehler", "Auszahlung konnte nicht gelöscht werden!" )
+	return HttpResponseRedirect( returnto )
--- urls.py
+++ urls.py
@@ -7,62 +7,61 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	# Example:
-	# (r'^carpediembanking/', include('carpediembanking.foo.urls')),
+    # Example:
+    # (r'^Kontenverwaltung/', include('Kontenverwaltung.foo.urls')),
 
-	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-	# to INSTALLED_APPS to enable admin documentation:
-	# (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-	# Uncomment the next line to enable the admin:
-	#(r'^carpediembanking/admin/(.*)',			admin.site.root),
+    # Uncomment the next line to enable the admin:
+    (r'^Kontenverwaltung/admin/(.*)',			admin.site.root),
 
-	# media
-	(r'^carpediembanking/static/(?P<path>.*)$',		'django.views.static.serve', {'document_root': '/var/www/Django/carpediembanking/static', 'show_indexes': True}), 
+    # media
+    #(r'^Kontenverwaltung/media/(?P<path>.*)$',		'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}), 
 
-	(r'^carpediembanking/$',				'Django.carpediembanking.views.Index'), # django.contrib.auth.views.login # ruft auf: registration/login.html
-	(r'^carpediembanking/login/$',				'Django.carpediembanking.views.Login'),
-	(r'^carpediembanking/logout/$',				'Django.carpediembanking.views.Logout'),
-	#    (r'^carpediembanking/IP$',				'Django.carpediembanking.views.IP'),
-	(r'^carpediembanking/ping$',				'Django.carpediembanking.views.ping'),
+    (r'^Kontenverwaltung/$',				'Kontenverwaltung.main.views.Index'), # django.contrib.auth.views.login # ruft auf: registration/login.html
+    (r'^Kontenverwaltung/login/$',			'Kontenverwaltung.main.views.Login'),
+    (r'^Kontenverwaltung/logout/$',			'Kontenverwaltung.main.views.Logout'),
+#    (r'^Kontenverwaltung/IP$',				'Kontenverwaltung.main.views.IP'),
+    (r'^Kontenverwaltung/ping$',			'Kontenverwaltung.main.views.ping'),
 
-	#    (r'^carpediembanking/Intranet/$',			'Django.carpediembanking.views.Baustelle'),
-	(r'^carpediembanking/Intranet/$',			'Django.carpediembanking.Intranet.Index'),
-	(r'^carpediembanking/Klientenkasse$',			'Django.carpediembanking.Intranet.Klientenkasse'),
-	(r'^carpediembanking/Zuteilung$',			'Django.carpediembanking.Intranet.Zuteilung'),
+#    (r'^Kontenverwaltung/Intranet/$',			'Kontenverwaltung.main.views.Baustelle'),
+    (r'^Kontenverwaltung/Intranet/$',			'Kontenverwaltung.main.Intranet.Index'),
+    (r'^Kontenverwaltung/Klientenkasse$',		'Kontenverwaltung.main.Intranet.Klientenkasse'),
+    (r'^Kontenverwaltung/Zuteilung$',			'Kontenverwaltung.main.Intranet.Zuteilung'),
 
-	(r'^carpediembanking/Klient$',				'Django.carpediembanking.Klient.Klient'),
-	(r'^carpediembanking/Forderungen$',			'Django.carpediembanking.Klient.Forderungen'),
-	(r'^carpediembanking/Einzahlung$',			'Django.carpediembanking.Klient.Einzahlung'),
-	(r'^carpediembanking/Auszahlung$',			'Django.carpediembanking.Klient.Auszahlung'),
-	(r'^carpediembanking/Ueberweisung$',			'Django.carpediembanking.Klient.Ueberweisung'),
-	(r'^carpediembanking/NeueSchulden$',			'Django.carpediembanking.Klient.NeueSchulden'),
-	(r'^carpediembanking/Entfernen$',			'Django.carpediembanking.Klient.Entfernen'),
+    (r'^Kontenverwaltung/Klient$',			'Kontenverwaltung.main.Klient.Klient'),
+    (r'^Kontenverwaltung/Forderungen$',			'Kontenverwaltung.main.Klient.Forderungen'),
+    (r'^Kontenverwaltung/Einzahlung$',			'Kontenverwaltung.main.Klient.Einzahlung'),
+    (r'^Kontenverwaltung/Auszahlung$',			'Kontenverwaltung.main.Klient.Auszahlung'),
+    (r'^Kontenverwaltung/Ueberweisung$',		'Kontenverwaltung.main.Klient.Ueberweisung'),
+    (r'^Kontenverwaltung/NeueSchulden$',		'Kontenverwaltung.main.Klient.NeueSchulden'),
+    (r'^Kontenverwaltung/Entfernen$',			'Kontenverwaltung.main.Klient.Entfernen'),
 
-	(r'^carpediembanking/Girokonto/$',			'Django.carpediembanking.Girokonto.Index'),
-	(r'^carpediembanking/Kontenumsaetze$',			'Django.carpediembanking.Girokonto.Kontenumsaetze'),
+    (r'^Kontenverwaltung/Girokonto/$',			'Kontenverwaltung.main.Girokonto.Index'),
+    (r'^Kontenverwaltung/Kontenumsaetze$',		'Kontenverwaltung.main.Girokonto.Kontenumsaetze'),
 
-	# die nachfolgende Aufzählung ist überholt
-	# evtl. aber noch hilfreich für die Teile, die wieder eingebunden werden
-	(r'^carpediembanking/Start$',				'Django.carpediembanking.views.start'),
-	(r'^carpediembanking/Betreute/Neu$',			'Django.carpediembanking.Betreute.Betreute.neu'),
-	(r'^carpediembanking/Betreute/Details$',		'Django.carpediembanking.Betreute.Betreute.details'),
-	(r'^carpediembanking/Betreute/Liste$',			'Django.carpediembanking.Betreute.Betreute.liste'),
-	(r'^carpediembanking/Betreute/Entfernen$',		'Django.carpediembanking.Betreute.Betreute.entfernen'),
-	(r'^carpediembanking/Geldeingaenge/Neu$',		'Django.carpediembanking.Geldeingaenge.Geldeingaenge.neu'),
-	(r'^carpediembanking/Geldeingaenge/Liste$',		'Django.carpediembanking.Geldeingaenge.Geldeingaenge.liste'),
-	(r'^carpediembanking/Geldeingaenge/Entfernen$',		'Django.carpediembanking.Geldeingaenge.Geldeingaenge.entfernen'),
-	(r'^carpediembanking/Zuteilungen/Neu$',			'Django.carpediembanking.Zuteilungen.Zuteilungen.neu'),
-	(r'^carpediembanking/Zuteilungen/Entfernen$',		'Django.carpediembanking.Zuteilungen.Zuteilungen.entfernen'),
-	(r'^carpediembanking/Auszahlungen/Neu$',		'Django.carpediembanking.Auszahlungen.Auszahlungen.neu'),
-	(r'^carpediembanking/Auszahlungen/Quittung$',		'Django.carpediembanking.Auszahlungen.Auszahlungen.quittung'),
-	(r'^carpediembanking/Auszahlungen/Liste$',		'Django.carpediembanking.Auszahlungen.Auszahlungen.liste'),
-	(r'^carpediembanking/Auszahlungen/Entfernen$',		'Django.carpediembanking.Auszahlungen.Auszahlungen.entfernen'),
-	(r'^carpediembanking/Forderungen/Neu$',			'Django.carpediembanking.Forderungen.Forderungen.neu'),
-	(r'^carpediembanking/Forderungen/Liste$',		'Django.carpediembanking.Forderungen.Forderungen.liste'),
-	(r'^carpediembanking/Forderungen/Entfernen$',		'Django.carpediembanking.Forderungen.Forderungen.entfernen'),
-	(r'^carpediembanking/Tilgungen/Neu$',			'Django.carpediembanking.Tilgungen.Tilgungen.neu'),
-	(r'^carpediembanking/Tilgungen/Entfernen$',		'Django.carpediembanking.Tilgungen.Tilgungen.entfernen'),
-
+# die nachfolgende Aufzählung ist überholt
+# evtl. aber noch hilfreich für die Teile, die wieder eingebunden werden
+    (r'^Kontenverwaltung/Start$',			'Kontenverwaltung.main.views.start'),
+    (r'^Kontenverwaltung/Betreute/Neu$',		'Kontenverwaltung.main.Betreute.Betreute.neu'),
+    (r'^Kontenverwaltung/Betreute/Details$',		'Kontenverwaltung.main.Betreute.Betreute.details'),
+    (r'^Kontenverwaltung/Betreute/Liste$',		'Kontenverwaltung.main.Betreute.Betreute.liste'),
+    (r'^Kontenverwaltung/Betreute/Entfernen$',		'Kontenverwaltung.main.Betreute.Betreute.entfernen'),
+    (r'^Kontenverwaltung/Geldeingaenge/Neu$',		'Kontenverwaltung.main.Geldeingaenge.Geldeingaenge.neu'),
+    (r'^Kontenverwaltung/Geldeingaenge/Liste$',		'Kontenverwaltung.main.Geldeingaenge.Geldeingaenge.liste'),
+    (r'^Kontenverwaltung/Geldeingaenge/Entfernen$',	'Kontenverwaltung.main.Geldeingaenge.Geldeingaenge.entfernen'),
+    (r'^Kontenverwaltung/Zuteilungen/Neu$',		'Kontenverwaltung.main.Zuteilungen.Zuteilungen.neu'),
+    (r'^Kontenverwaltung/Zuteilungen/Entfernen$',	'Kontenverwaltung.main.Zuteilungen.Zuteilungen.entfernen'),
+    (r'^Kontenverwaltung/Auszahlungen/Neu$',		'Kontenverwaltung.main.Auszahlungen.Auszahlungen.neu'),
+    (r'^Kontenverwaltung/Auszahlungen/Quittung$',	'Kontenverwaltung.main.Auszahlungen.Auszahlungen.quittung'),
+    (r'^Kontenverwaltung/Auszahlungen/Liste$',		'Kontenverwaltung.main.Auszahlungen.Auszahlungen.liste'),
+    (r'^Kontenverwaltung/Auszahlungen/Entfernen$',	'Kontenverwaltung.main.Auszahlungen.Auszahlungen.entfernen'),
+    (r'^Kontenverwaltung/Forderungen/Neu$',		'Kontenverwaltung.main.Forderungen.Forderungen.neu'),
+    (r'^Kontenverwaltung/Forderungen/Liste$',		'Kontenverwaltung.main.Forderungen.Forderungen.liste'),
+    (r'^Kontenverwaltung/Forderungen/Entfernen$',	'Kontenverwaltung.main.Forderungen.Forderungen.entfernen'),
+    (r'^Kontenverwaltung/Tilgungen/Neu$',		'Kontenverwaltung.main.Tilgungen.Tilgungen.neu'),
+    (r'^Kontenverwaltung/Tilgungen/Entfernen$',		'Kontenverwaltung.main.Tilgungen.Tilgungen.entfernen'),
 )
 
