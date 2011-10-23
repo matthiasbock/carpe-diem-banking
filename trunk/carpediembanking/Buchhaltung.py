0d4b221607f0d4ba9cf282e0cab2c3d5fcbcc21f
matthias.bock@hu-berlin.de <matthias.bock@hu-berlin.de@671eb7a7-5a69-cd39-6f2f-afc427a5595e>
2011-10-23 19:16:38 +0000

    decorators

--- trunk/carpediembanking/Buchhaltung.py
+++ trunk/carpediembanking/Buchhaltung.py
@@ -1,18 +1,17 @@
 # -*- coding: iso-8859-15 -*-
 
-from Django.carpediembanking.models import *
-from Django.carpediembanking.includes import *
+from models import *
+from includes import *
+from decorators import global_decorator
 
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.decorators.cache import cache_control
-from django.contrib.auth.decorators import login_required
 
-@cache_control( max_age=1, must_revalidate=True, no_cache=True )
-#@login_required
+@global_decorator
 def Index( request ):
 	return HttpResponseRedirect("/Django/carpediembanking/Klientenkasse")
 
+@global_decorator
 def Klientenkasse( request ):
 	params = defaults( request )
 	params["Klientenkasse"] = True
@@ -33,6 +32,7 @@ def Klientenkasse( request ):
 	# Überweisungen (Überweisung.FuerKlient==None)
 	return render_to_response("Klientenkasse.html", params)
 
+@global_decorator
 def Zuteilung( request ):
 	params = defaults( request )
 	return render_to_response("Baustelle.html", params)
---
+++ trunk/carpediembanking/decorators.py
@@ -0,0 +1,18 @@
+# -*- coding: iso-8859-15 -*-
+
+from django.views.decorators.cache import cache_control
+from django.contrib.auth.decorators import login_required
+
+def auth_ip_decorator(the_func):
+    """
+    Make another a function more beautiful.
+    """
+    def _decorated(*args, **kwargs):
+        return the_func(*args, **kwargs)
+    return _decorated
+
+
+
+@cache_control( max_age=1, must_revalidate=True, no_cache=True )
+@login_required
+@auth_ip_required
